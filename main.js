0d049a75bf456bce0ce6faf1bd5de51f0e84d94b
klarrevs <klara.vadel@medieinstitutet.se>
2024-11-03 13:53:03 +0100

    delete first game idea

--- main.js
+++ main.js
@@ -3,12 +3,3 @@ window.addEventListener("DOMContentLoaded", main);
 function main() {
 console.log("Start of the program");
 }
-
-const character = document.getElementById ("character");
-const room1 = document.getElementById ("room1");
-const room2 = document.getElementById ("room2");
-const treasureChest = document.getElementById ("treasureChest");
-const coralReef = document.getElementById ("coralReef");
-const fish1 = document.getElementById ("fish1");
-const fish2 = document.getElementById ("fish2");
-const fish3 = document.getElementById ("fish3");
