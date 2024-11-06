6cc4c887a4da7a6478b8fd7883562ca9109a467a
klarrevs <klara.vadel@medieinstitutet.se>
2024-11-06 15:32:11 +0100

    add image and change button in function

--- main.js
+++ main.js
@@ -1,52 +1,79 @@
-window.addEventListener("DOMContentLoaded", main);
+window.addEventListener("DOMContentLoaded", loadMainScene);
 
-const sceneText = document.getElementById('sceneText');
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
+const sceneText = document.getElementById("sceneText");
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
 
-function main() {
-    document.body.style.backgroundImage = "url('../Assets/startingscene.webp')";
-    sceneText.textContent = 'You are at a party and this fella says to you: "Something bad is happening, I\'m sure you do agree. People care for nothing, no respect for human rights. Evil times are coming, we are in for darker nights." What is your next move?';
-    button1.textContent = 'Answer';
-    button2.textContent = 'Join Amnesty';
+function loadMainScene() {
+  document.body.style.backgroundImage = "url('../Assets/startingscene.webp')";
+  sceneText.textContent =
+    'You are at a party and this fella says to you: "Something bad is happening, I\'m sure you do agree. People care for nothing, no respect for human rights. Evil times are coming, we are in for darker nights." What is your next move?';
+  button1.textContent = "Answer";
+  button2.textContent = "Join Amnesty";
 
-    button1.onclick = ImpendingDoomScene;
-    button2.onclick = AmnestyScene;
+  button1.style.display = null;
+
+  button1.onclick = loadImpendingDoomScene;
+  button2.onclick = loadAmnestyScene;
 }
 
-function AmnestyScene() {
-    document.body.style.backgroundImage = "url('../Assets/amnesty.jpg')";
-    sceneText.textContent = 'You have joined Amnesty.';
-    button1.style.display = 'none';
-    button2.textContent = 'Start over';
+function loadAmnestyScene() {
+  document.body.style.backgroundImage = "url('../Assets/amnesty.jpg')";
+  sceneText.textContent = "You have joined Amnesty.";
+  button1.style.display = "none";
+  button2.textContent = "Start over";
 
-    button2.onclick = main;
+  button2.onclick = loadMainScene;
 }
 
-function ImpendingDoomScene() {
-    document.body.style.backgroundImage = "url('../Assets/impendingdoom.webp')";
-    sceneText.textContent = 'You say "Who are you to talk about an impending doom?" He gets kinda wary as he looks around the room. He says "I\'m a minister, a big shot in the state..."';
-    button1.textContent = 'Answer';
-    button2.textContent = 'Ignore';
+function loadImpendingDoomScene() {
+  document.body.style.backgroundImage = "url('../Assets/impendingdoom.webp')";
+  sceneText.textContent =
+    'You say: "Who are you to talk about an impending doom?" He gets kinda wary as he looks around the room. He says: "I\'m a minister, a big shot in the state..."';
+  button1.textContent = "Answer";
+  button2.textContent = "Ignore";
 
-    button1.onclick = loadWhatIsWrongWhatIsRightScene;
-    button2.onclick = OtherGuyScene;    
+  button1.onclick = loadWhatIsWrongWhatIsRightScene;
+  button2.onclick = loadOtherGuyScene;
 }
 
+function loadWhatIsWrongWhatIsRightScene() {
+  document.body.style.backgroundImage = "url('../Assets/whatiswrongwhatisright.webp')";
+  sceneText.textContent =
+    'You say: "I just can\'t believe it, boy I think it\'s great. Brother can you tell me what is right and what is wrong?" He says: "Keep on rocking baby, till the night is gone"';
+  button1.textContent = "Keep on rocking";
+  button2.textContent = "Leave to play BlackJack";
 
-
-function loadWhatIsWrongWhatIsRightScene(){
-
+  button1.onclick = loadDanceFloorScene;
+  button2.onclick = loadBlackJackScene;
 }
 
-function OtherGuyScene() {
-    document.body.style.backgroundImage = "url('../Assets/otherguy.jpeg')";
-    // body.style.justifyContent = 'flex-start';
-    sceneText.textContent = 'Over in the corner, you can see this other guy. He is kinda flirty, he is giving you the eye.';
-    button1.textContent = 'Take advantage of the situation';
-    button2.textContent = 'Leave to play BlackJack';
-    
-    button1.onclick = takeAdvantageOfTheSituationScene;
-    button2.onclick = PlayBlackJackScene;
+function loadOtherGuyScene() {
+  document.body.style.backgroundImage = "url('../Assets/otherguy.jpeg')";
+  sceneText.textContent =
+    "Over in the corner, you can see this other guy. He is kinda flirty, he is giving you the eye.";
+  button1.textContent = "Take advantage of the situation";
+  button2.textContent = "Leave to play BlackJack";
 
+  button1.onclick = loadTakeAdvantageOfTheSituationScene;
+  button2.onclick = loadBlackJackScene;
 }
+
+function loadTakeAdvantageOfTheSituationScene() {
+  document.body.style.backgroundImage = "url('../Assets/advantageofthesituation.webp')";
+  sceneText.textContent = 
+    "So you take advantage of the fact that you're a star. You shake your hair and take a casual stroll up to the bar. And as sure as hell this guy was coming up to you. He says: \"Who am I and who are you and who are we? What's our situation, do we have some time for us?\" What will your answer be?";
+  button1.textContent = "I was not exactly waiting for the bus.";
+  button2.textContent = "Nope!";
+
+  button1.onclick =
+  button2.onclick = loadDanceFloorScene;
+}
+
+// function loadBlackJackScene() {
+//     document.body.style.backgroundImage =
+
+// }
+// function loadDanceFloorScene(){
+
+//}
